7ddbbf094a9ab2616c8656eebe4dd6f78b99ef45
CameronKerr <51450639+CameronKerr@users.noreply.github.com>
2019-11-18 19:53:52 -0500

    Update CloneTracking.R

--- Clone Track Functions/CloneTracking.R
+++ Clone Track Functions/CloneTracking.R
@@ -11,7 +11,7 @@ library(randomcoloR)
 # @param chain: Desired chain to analyze, could be TRA, TRB, TRD, TRG
 # @param clnefrc: cut-off from 0 to 1 to track and plot only a subset of clonotypes
 # @param tracking: Name of the file which contains the desired tracking clonotypes
-# @param
+# @param src: Path to MiXCR files. Must have the form of a folder with multiple sub-folders of each patient with the specific sample cohort starting with "CLONES_"
 
 
 clonetrack.fx <- function(patient, sampcohort, chain, clnefrc, tracking, src){
